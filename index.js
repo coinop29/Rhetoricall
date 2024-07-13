f59a7ba2fc7a1adc6eaaa664cb107f1ffa7e33c3
asadahmad27 <amasadmughal@gmail.com>
2024-07-13 13:44:11 +0500

    pull from repo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require('path');
+const path = require("path");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
@@ -9,8 +9,8 @@ require("dotenv").config();
 const { init, insertItem, checkPhoneNumber } = require("./db");
 const { initTwilio } = require("./twilio");
 const routes = require("./routes");
-const { MessagingResponse } = require('twilio').twiml;
-const urlBodyParser = express.urlencoded({ extended: false});
+const { MessagingResponse } = require("twilio").twiml;
+const urlBodyParser = express.urlencoded({ extended: false });
 const Joi = require("@hapi/joi");
 const Filter = require("bad-words");
 const filter = new Filter();
@@ -18,6 +18,8 @@ const filter = new Filter();
 var corsOptions = {
   origin: "*",
 };
+
+const PORT = process.env.PORT || 5000;
 
 const itemSchema = Joi.object().keys({
   sid: Joi.string(),
@@ -30,7 +32,7 @@ const itemSchema = Joi.object().keys({
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(routes);
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 init();
 initTwilio();
@@ -42,6 +44,12 @@ const io = require("socket.io")(server, {
   },
 });
 
+// const io = require("socket.io")(http, {
+//   cors: {
+//     origin: "*",
+//   },
+// });
+
 io.on("connection", (socket) => {
   console.log("new connection");
   socket.on("history", (msg) => {
@@ -50,9 +58,9 @@ io.on("connection", (socket) => {
   });
 });
 
-app.post("/api/messageIncoming", urlBodyParser, async(req, res) => {
-  console.log('request =========>', req);
-  console.log('request body =========>', req.body);
+app.post("/api/messageIncoming", urlBodyParser, async (req, res) => {
+  console.log("request =========>", req);
+  console.log("request body =========>", req.body);
   const twiml = new MessagingResponse();
   // AccountSid, NumMedia, NumSegments, RefferralNumMedia, , FromCity, FromCountry, FromState, FromZip, MessageSid, SmsMessageSid, ToCity, ToCountry, Tostate, ToZip, SmsStatus
   const { Body, From, SmsSid, To } = req.body;
@@ -62,36 +70,34 @@ app.post("/api/messageIncoming", urlBodyParser, async(req, res) => {
     to: To,
     body: Body,
     filtered: filter.clean(Body),
-  }
-  
+  };
+
   const result = itemSchema.validate(item);
-  console.log('result ======>', result, item);
-  if(result.error){
-    twiml.message('Invalid data type');
-    res.type('text/xml').send(twiml.toString());
+  console.log("result ======>", result, item);
+  if (result.error) {
+    twiml.message("Invalid data type");
+    res.type("text/xml").send(twiml.toString());
     return;
   }
   const isExists = await checkPhoneNumber(item.from);
-  
+
   insertItem(item)
     .then(() => {
-      console.log('item saved');
+      console.log("item saved");
       io.emit("messageIncoming", item);
-      if(isExists){
-        res.status(200).send('success');
-      } else{
-        twiml.message('Thanks for your contribution!');
-        res.type('text/xml').send(twiml.toString());
+      if (isExists) {
+        res.status(200).send("success");
+      } else {
+        twiml.message("Thanks for your contribution!");
+        res.type("text/xml").send(twiml.toString());
       }
     })
     .catch((error) => {
       console.log(error);
       res.status(500).end();
-    })
-  
+    });
 });
 
-
-server.listen(process.env.PORT || 5000, () => {
-  console.log(`server is running on port ${process.env.PORT}`);
+server.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
 });
