966ce87e2e05830b39678cb330f97788c18f3ab5
asadahmad27 <amasadmughal@gmail.com>
2024-07-22 16:47:30 +0500

    bad words filter update v2

--- bad-words-hacked.js
+++ bad-words-hacked.js
@@ -1,20 +1,45 @@
-const Filter = require("bad-words");
+// const Filter = require("bad-words");
+
+// class FilterHacked extends Filter {
+//   cleanHacked(string) {
+//     try {
+//       return this.clean(string);
+//     } catch (error) {
+//       const joinMatch = this.splitRegex.exec(string);
+//       const joinString = (joinMatch && joinMatch[0]) || "";
+//       return string
+//         .split(this.splitRegex)
+//         .map((word) => {
+//           return this.isProfane(word) ? this.replaceWord(word) : word;
+//         })
+//         .join(joinString);
+//     }
+//   }
+// }
+
+// module.exports = FilterHacked;
+
+// filename: bad-words-hacked.js
+const Filter = require('bad-words');
 
 class FilterHacked extends Filter {
-  cleanHacked(string) {
-    try {
-      return this.clean(string);
-    } catch (error) {
-      const joinMatch = this.splitRegex.exec(string);
-      const joinString = (joinMatch && joinMatch[0]) || "";
-      return string
-        .split(this.splitRegex)
-        .map((word) => {
-          return this.isProfane(word) ? this.replaceWord(word) : word;
-        })
-        .join(joinString);
+    cleanHacked(string) {
+        try {
+            // Check if the string contains only emojis
+            const isOnlyEmoji = string.replace(/[\p{Emoji_Presentation}]/gu, '').trim().length === 0;
+            if (isOnlyEmoji) {
+                return string; // Return the original string if it is only emojis
+            }
+            return this.clean(string);
+        } catch (error) {
+            const joinMatch = this.splitRegex.exec(string);
+            const joinString = (joinMatch && joinMatch[0]) || '';
+            return string.split(this.splitRegex).map((word) => {
+                const containsNonEmoji = word.replace(/[\p{Emoji_Presentation}]/gu, '').trim().length > 0;
+                return containsNonEmoji && this.isProfane(word) ? this.replaceWord(word) : word;
+            }).join(joinString);
+        }
     }
-  }
 }
 
 module.exports = FilterHacked;
